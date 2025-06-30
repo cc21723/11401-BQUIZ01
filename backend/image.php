288bcbdffa46e4d14595f3c3e75d09233a2ce15e
mackliu <macklun@gmail.com>
2025-06-30 11:02:20 +0800

    完成校園映象分頁功能

--- backend/image.php
+++ backend/image.php
@@ -40,7 +40,21 @@
                 ?>
             </tbody>
         </table>
+       <div class='cent'>
+                <?php if($now-1>0): ?>
+                 <a href='?do=<?=$do;?>&p=<?=$now-1;?>'>< </a>
+                <?php endif ;?>
+                 
+                <?php for($i=1;$i<=$pages;$i++):
+                     $size=($now==$i)?'24px':''; 
+                ?>
+                 <a href='?do=<?=$do;?>&p=<?=$i;?>' style="font-size:<?=$size;?>"> <?=$i;?> </a>
+                <?php endfor;?>
 
+                <?php if($now+1<=$pages):?>
+                 <a href='?do=<?=$do;?>&p=<?=$now+1;?>'>></a>
+                <?php endif ;?>
+       </div>
         <table style="margin-top:40px; width:70%;">
             <tbody>
                 <tr>
