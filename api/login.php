2013022724603f8622b1fb96bc0c5985b2f01458
mackliu <macklun@gmail.com>
2025-07-04 09:48:04 +0800

    增加對session的判斷,更改管理登入按鈕為返回管理按鈕

--- api/login.php
+++ api/login.php
@@ -7,6 +7,7 @@ $user=$Admin->count($_POST);
 
 //if(!empty($user)){
 if($user){
+    $_SESSION['login']=1;
     to("../backend.php");
 }else{
     echo "<script>alert('帳號或密碼錯誤');location.replace('../index.php?do=login')</script>";
